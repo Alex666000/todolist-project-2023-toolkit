1b955e19b8488d6cd2a798ae9d5b377bf1a073f6
Aleksandr_UB <bogdanov777000@mail.ru>
2023-05-14 16:47:54 +0300

    end

--- src/features/todolists-list/todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists-list/todolists/Todolist/Tasks/Tasks.tsx
@@ -1,69 +1,59 @@
-import React, {FC} from "react";
-import {Task} from "features/todolists-list/todolists/Todolist/Tasks/Task/Task";
-import {TaskStatuses} from "common/enums";
-import {TodolistDomainType} from "features/todolists-list/todolists/todolists.reducer";
-import {TaskType} from "features/todolists-list/tasks/tasks.api";
-import {makeStyles} from "@mui/styles";
+import React, { FC } from "react"
+import { Task } from "features/todolists-list/todolists/Todolist/Tasks/Task/Task"
+import { TaskStatuses } from "common/enums"
+import { TodolistDomainType } from "features/todolists-list/todolists/todolists.reducer"
+import { TaskType } from "features/todolists-list/tasks/tasks.api"
+import { makeStyles } from "@mui/styles"
 
-
-type Classes = ReturnType<typeof useStyles>;
+type Classes = ReturnType<typeof useStyles>
 
 const useStyles = makeStyles({
-    noTasks: {
-        padding: "10px",
-        color: "#3363dd",
-        opacity: 0.7,
-        fontSize: "14px",
-        position: 'relative',
-        animation: "$moveUpDown 5s ease-in-out infinite"
-    },
+  noTasks: {
+    padding: "10px",
+    color: "#3363dd",
+    opacity: 0.7,
+    fontSize: "14px",
+    position: "relative",
+    animation: "$moveUpDown 5s ease-in-out infinite",
+  },
 
-    "@keyframes moveUpDown": {
-        "0%": {
-            opacity: 0
-        },
-        "50%": {
-            opacity: 0.5
-        },
-        "100%": {
-            opacity: 1
-        }
-    }
+  "@keyframes moveUpDown": {
+    "0%": {
+      opacity: 0,
+    },
+    "50%": {
+      opacity: 0.5,
+    },
+    "100%": {
+      opacity: 1,
+    },
+  },
 })
 
 type Props = {
-    todolist: TodolistDomainType;
-    tasks: TaskType[];
-};
+  todolist: TodolistDomainType
+  tasks: TaskType[]
+}
 
-export const Tasks: FC<Props> = ({tasks, todolist}) => {
-    const classes: Classes = useStyles();
+export const Tasks: FC<Props> = ({ tasks, todolist }) => {
+  const classes: Classes = useStyles()
 
-    let tasksForTodolist: TaskType[] = tasks;
+  let tasksForTodolist: TaskType[] = tasks
 
-    if (todolist.filter === "active") {
-        tasksForTodolist = tasks.filter((t) => t.status === TaskStatuses.New);
-    }
+  if (todolist.filter === "active") {
+    tasksForTodolist = tasks.filter(t => t.status === TaskStatuses.New)
+  }
 
-    if (todolist.filter === "completed") {
-        tasksForTodolist = tasks.filter(
-            (t) => t.status === TaskStatuses.Completed
-        );
-    }
+  if (todolist.filter === "completed") {
+    tasksForTodolist = tasks.filter(t => t.status === TaskStatuses.Completed)
+  }
 
-    return (
-        <>
-            {tasksForTodolist.map((t) => (
-                <Task key={t.id} task={t} todolistId={todolist.id}/>
-            ))}
-            {tasksForTodolist.length === 0 && (
-                <div className={classes.noTasks}>No tasks: Please add tasks</div>
-            )}
-        </>
-    );
-};
-
-
-
-
-
+  return (
+    <>
+      {tasksForTodolist.map(t => (
+        <Task key={t.id} task={t} todolistId={todolist.id} />
+      ))}
+      {tasksForTodolist.length === 0 && <div className={classes.noTasks}>No tasks: Please add tasks</div>}
+    </>
+  )
+}
--- src/features/todolists-list/todolists/Todolist/Todolist.tsx
+++ src/features/todolists-list/todolists/Todolist/Todolist.tsx
@@ -1,69 +1,64 @@
-import React, {FC, memo, useEffect} from "react"
+import React, { FC, memo, useEffect } from "react"
 import {
-    FilterValuesType,
-    TodolistDomainType,
-    todolistsActions
+  FilterValuesType,
+  TodolistDomainType,
+  todolistsActions,
 } from "features/todolists-list/todolists/todolists.reducer"
-import {tasksThunks} from "features/todolists-list/tasks/tasks.reducer"
-import {useActions} from "common/hooks"
-import {AddItemForm} from "common/components"
-import {TaskType} from "features/todolists-list/tasks/tasks.api"
-import {Tasks} from "features/todolists-list/todolists/Todolist/Tasks/Tasks"
-import {TodolistTitle} from "features/todolists-list/todolists/Todolist/TodolistTitle/TodolistTitle"
-import {Button, Paper} from "@mui/material";
-import {OverridableStringUnion} from "@mui/types";
+import { tasksThunks } from "features/todolists-list/tasks/tasks.reducer"
+import { useActions } from "common/hooks"
+import { AddItemForm } from "common/components"
+import { TaskType } from "features/todolists-list/tasks/tasks.api"
+import { Tasks } from "features/todolists-list/todolists/Todolist/Tasks/Tasks"
+import { TodolistTitle } from "features/todolists-list/todolists/Todolist/TodolistTitle/TodolistTitle"
+import { Button, Paper } from "@mui/material"
+import { OverridableStringUnion } from "@mui/types"
 
 type Props = {
-    todolist: TodolistDomainType
-    tasks: TaskType[]
+  todolist: TodolistDomainType
+  tasks: TaskType[]
 }
-export const Todolist: FC<Props> = memo((props) => {
-    const {changeTodolistFilter} = useActions(todolistsActions)
+export const Todolist: FC<Props> = memo(props => {
+  const { changeTodolistFilter } = useActions(todolistsActions)
 
-    const {fetchTasks, addTask} = useActions(tasksThunks)
+  const { fetchTasks, addTask } = useActions(tasksThunks)
 
-    useEffect(() => {
-        fetchTasks(props.todolist.id)
-    }, [])
+  useEffect(() => {
+    fetchTasks(props.todolist.id)
+  }, [])
 
-    const addTaskCallback = (title: string) => {
-        // .unwrap() -- чтобы не зачищалось поле когда ввели ошибку
-        return addTask({title, todolistId: props.todolist.id}).unwrap()
-    }
+  const addTaskCallback = (title: string) => {
+    // .unwrap() -- чтобы не зачищалось поле когда ввели ошибку
+    return addTask({ title, todolistId: props.todolist.id }).unwrap()
+  }
 
-    const onAllClickHandler = (filter: FilterValuesType) => {
-        changeTodolistFilter({filter, id: props.todolist.id})
-    }
+  const onAllClickHandler = (filter: FilterValuesType) => {
+    changeTodolistFilter({ filter, id: props.todolist.id })
+  }
 
-    // renderFunction
-    const renderFilterButton = (
-        onClick: () => void,
-        buttonFilter: FilterValuesType,
-        color: OverridableStringUnion<'inherit' | 'primary' | 'secondary'>,
-        text: string) => {
-        return (
-            <Button
-                variant={props.todolist.filter === buttonFilter ? "outlined" : "text"}
-                onClick={onClick}
-                color={color}
-            >
-                {text}
-            </Button>
-        )
-    }
-
+  // renderFunction
+  const renderFilterButton = (
+    onClick: () => void,
+    buttonFilter: FilterValuesType,
+    color: OverridableStringUnion<"inherit" | "primary" | "secondary">,
+    text: string,
+  ) => {
     return (
-        <Paper style={{padding: "10px"}}>
-            <TodolistTitle todolist={props.todolist}/>
-            <AddItemForm addItem={addTaskCallback} disabled={props.todolist.entityStatus === "loading"}/>
-            <Tasks todolist={props.todolist} tasks={props.tasks}/>
-            <div style={{paddingTop: "10px"}}>
-                {renderFilterButton(() => onAllClickHandler("all"), 'all', 'inherit', 'All')}
-                {renderFilterButton(() => onAllClickHandler("active"), 'active', 'primary', 'Active')}
-                {renderFilterButton(() => onAllClickHandler("completed"), 'completed', 'secondary', 'Completed')}
-            </div>
-        </Paper>
+      <Button variant={props.todolist.filter === buttonFilter ? "outlined" : "text"} onClick={onClick} color={color}>
+        {text}
+      </Button>
     )
+  }
+
+  return (
+    <Paper style={{ padding: "10px" }}>
+      <TodolistTitle todolist={props.todolist} />
+      <AddItemForm addItem={addTaskCallback} disabled={props.todolist.entityStatus === "loading"} />
+      <Tasks todolist={props.todolist} tasks={props.tasks} />
+      <div style={{ paddingTop: "10px" }}>
+        {renderFilterButton(() => onAllClickHandler("all"), "all", "inherit", "All")}
+        {renderFilterButton(() => onAllClickHandler("active"), "active", "primary", "Active")}
+        {renderFilterButton(() => onAllClickHandler("completed"), "completed", "secondary", "Completed")}
+      </div>
+    </Paper>
+  )
 })
-
-
