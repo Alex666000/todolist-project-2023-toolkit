9ce35303c748c5fe0cb0f72ade2a53758a206b73
Aleksandr_UB <bogdanov777000@mail.ru>
2023-05-22 17:32:35 +0300

    change API-KEY

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    "API-KEY": "35a2bc33-9361-4658-a8b4-b1a6a75da53d",
   },
 })
