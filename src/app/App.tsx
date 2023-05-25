caa234261529d461c976984311c75d0054f23d3f
Aleksandr_UB <bogdanov777000@mail.ru>
2023-05-25 17:00:25 +0300

    add gp

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,50 +1,47 @@
-import React, {useEffect} from "react"
-import {useSelector} from "react-redux"
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import {CircularProgress, Container} from "@mui/material"
-import {Login} from "features/auth/Login/Login"
-import {TodolistsList} from "features/todolists-list/TodolistsList"
-import {ErrorSnackbar} from "common/components"
-import {useActions} from "common/hooks"
-import {selectAppStatus, selectIsInitialized} from "app/app.selectors"
-import {authThunks} from "features/auth/auth.reducer"
+import React, { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { CircularProgress, Container } from "@mui/material"
+import { Login } from "features/auth/Login/Login"
+import { TodolistsList } from "features/todolists-list/TodolistsList"
+import { ErrorSnackbar } from "common/components"
+import { useActions } from "common/hooks"
+import { selectIsInitialized } from "app/app.selectors"
+import { authThunks } from "features/auth/auth.reducer"
+import { HeaderAppBar } from "app/HeaderAppBar"
 import "./App.css"
-import {HeaderAppBar} from "app/HeaderAppBar";
-
 
 function App() {
-    const isInitialized = useSelector(selectIsInitialized)
+  const isInitialized = useSelector(selectIsInitialized)
 
-    const {initializeApp} = useActions(authThunks)
+  const { initializeApp } = useActions(authThunks)
 
-    useEffect(() => {
-        initializeApp({})
-    }, [])
+  useEffect(() => {
+    initializeApp({})
+  }, [])
 
-    if (!isInitialized) {
-        return (
-            <div style={{position: "fixed", top: "30%", textAlign: "center", width: "100%"}}>
-                <CircularProgress/>
-            </div>
-        )
-    }
-
+  if (!isInitialized) {
     return (
-        <BrowserRouter>
-            <div className='App'>
-                <ErrorSnackbar/>
-                <HeaderAppBar/>
-                <Container
-                    sx={{maxWidth: '100vw', minHeight: '100vh', margin: 0}}
-                    fixed>
-                    <Routes>
-                        <Route path={"/"} element={<TodolistsList/>}/>
-                        <Route path={"/login"} element={<Login/>}/>
-                    </Routes>
-                </Container>
-            </div>
-        </BrowserRouter>
+      <div style={{ position: "fixed", top: "30%", textAlign: "center", width: "100%" }}>
+        <CircularProgress />
+      </div>
     )
+  }
+
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <ErrorSnackbar />
+        <HeaderAppBar />
+        <Container sx={{ maxWidth: "100vw", minHeight: "100vh", margin: 0 }} fixed>
+          <Routes>
+            <Route path={"/"} element={<TodolistsList />} />
+            <Route path={"/login"} element={<Login />} />
+          </Routes>
+        </Container>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
